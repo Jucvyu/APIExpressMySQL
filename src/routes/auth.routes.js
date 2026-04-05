43fd7e5ac775975f205264619f3fac69d6458664
Jucvyu <99603654+Jucvyu@users.noreply.github.com>
2026-04-05 13:55:38 -0500

    update routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,10 +2,6 @@ const router = require("express").Router();
 const auth = require("../controllers/auth.controller");
 const verifyToken = require("../middleware/auth.middleware");
 
-router.post("/register", (req, res) => {
-    console.log("RUTA OK");
-    res.json({ ok: true });
-});
 router.post("/register", auth.register);
 router.post("/login", auth.login);
 
