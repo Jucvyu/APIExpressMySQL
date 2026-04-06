3a7d8afda76dbb41933bc9af77fc2c6d4e0c5613
Jucvyu <99603654+Jucvyu@users.noreply.github.com>
2026-04-06 06:28:43 -0500

    Remove Authheader middleware

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,12 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-    const authHeader = req.headers["authorization"];
-
-    if (!authHeader)
-        return res.status(401).json({ message: "No token" });
-
-    const token = authHeader.split(" ")[1];
+    const token = req.headers["authorization"];
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
