89e12723ad3c454e56943f8cee5e8159ef41b715
Jucvyu <99603654+Jucvyu@users.noreply.github.com>
2026-04-06 08:45:54 -0500

    Update routes

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -3,7 +3,7 @@ const user = require("../controllers/users.controller");
 const verifyToken = require("../middleware/auth.middleware");
 const roleMiddleware = require("../middleware/role.middleware");
 
-router.get("/", verifyToken, user.getUsers);
+router.get("/", verifyToken, roleMiddleware(["admin"]), user.getUsers);
 router.get("/:id", verifyToken, user.getUser);
 router.put("/:id", verifyToken, user.updateUser);
 router.delete("/:id", verifyToken, roleMiddleware(["admin"]), user.deleteUser);
